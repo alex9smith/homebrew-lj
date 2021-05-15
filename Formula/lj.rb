e1c96df7dc533b9abe9372a1de5f488d4c372ab3
Alex Smith <alex@alexsmith.dev>
2021-05-15 16:24:18 +0100

    Fix SHA

--- Formula/lj.rb
+++ Formula/lj.rb
@@ -2,7 +2,7 @@ class Lj < Formula
   desc "Like `ls`, but outputs JSON!"
   homepage "https://github.com/alex9smith/lj"
   url "https://github.com/alex9smith/lj/releases/download/1.0.0/lj-1.0.0-apple-intel.tar.gz"
-  sha256 "559364c97b22621b0fa88a272f3595706f9a243e"
+  sha256 "da77514eca51d6384a2e73e516d9cbab8f4c846d9988185014956f49d24c0f83"
   version "1.0.0"
 
   def install
